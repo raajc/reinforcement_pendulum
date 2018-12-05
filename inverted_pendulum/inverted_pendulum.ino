170e99823edd23c0a1cbfbd3dae189f676379119
Raaj Chatterjee <raajc@sfu.ca>
2018-12-05 12:33:58 -0800

    Create inverted_pendulum.ino
    Added arduino file with basic PWM write, duty cycle sweep capability.

---
+++ inverted_pendulum/inverted_pendulum.ino
@@ -0,0 +1,21 @@
+int pwm_pin = 6;
+int i = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(pwm_pin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  pwmWrite(pwm_pin);
+
+  
+}
+
+void pwmWrite(int pwm_pin) {
+    for(i = 1; i < 255; i++) {
+    analogWrite(pwm_pin, i); 
+    delay(10);
+  }
+}
