0c3c4dd484ca51f1d6d4e78f63b5ef8833c5cdfb
Raaj Chatterjee <raajc@sfu.ca>
2018-12-06 00:26:17 -0800

    Added derivative gain, motor encoder to pins 4 and 5, untested

--- inverted_pendulum/inverted_pendulum.ino
+++ inverted_pendulum/inverted_pendulum.ino
@@ -1,18 +1,25 @@
 // Encoder pins
 const byte encoderPinA = 2;//outputA digital pin2
 const byte encoderPinB = 3;//outoutB digital pin3
+const byte motorcoderPinA = 4;//outputA digital pin4
+const byte motorcoderPinB = 5;//outoutB digital pin5
 
 //Encoder counts
 volatile int count = 0;
+volatile int motor_count = 0;
 int protectedCount = 0;
 int previousCount = 0;
 
 double kp = 15;
 double kd = 1;
 int offset = 0; //out of 255
+int diff = 0;
+int count_prev = 0;
 //Encoder Interrupts
 #define readA digitalRead(2)
 #define readB digitalRead(3)
+#define motoreadA digitalRead(4)
+#define motoreadB digitalRead(5)
 
 //PWM pins
 int pwm_pinL = 6;
@@ -37,9 +44,14 @@ Serial.begin (115200);
 
 pinMode(encoderPinA, INPUT_PULLUP);
 pinMode(encoderPinB, INPUT_PULLUP);
+pinMode(motorcoderPinA, INPUT_PULLUP);
+pinMode(motorcoderPinB, INPUT_PULLUP);
 
 attachInterrupt(digitalPinToInterrupt(encoderPinA), isrA, CHANGE);
 attachInterrupt(digitalPinToInterrupt(encoderPinB), isrB, CHANGE);
+
+attachInterrupt(digitalPinToInterrupt(motorcoderPinA), m_isrA, CHANGE);
+attachInterrupt(digitalPinToInterrupt(motorcoderPinB), m_isrB, CHANGE);
 
 }
 
@@ -54,13 +66,15 @@ void loop() {
 if(millis()- control_loop_timer > control_loop_period) { 
   control_loop_timer = millis();
   // Print encoder if different from previous value
+  diff = count - count_prev;
   if (count > 0) {
-    output = kp*(count)+offset;
+    output = kp*(count)+offset+kd*diff;
     motorL(pwm_pinL, pwm_pinR, min(output,255));
   } else {
-    output = -kp*(count)+offset;
+    output = -kp*(count)+offset+kd*diff;
     motorR(pwm_pinL, pwm_pinR, min(output, 255));
   }
+  count_prev = count;
 }
 
 if(millis() - readout_loop_timer> readout_loop_period) { 
@@ -85,6 +99,7 @@ void motorR(int pwm_pinL, int pwm_pinR, int duty) {
     analogWrite(pwm_pinR, duty); 
 }
 
+// Encoder ISRs
 void isrA() {
   if(readB != readA) {
     count ++;
@@ -97,5 +112,21 @@ void isrB() {
     count ++;
   } else {
     count --;
+  }
+}
+
+// Motor Encoder ISRs
+void m_isrA() {
+  if(motoreadB != motoreadA) {
+    motor_count ++;
+  } else {
+    motor_count --;
+  }
+}
+void m_isrB() {
+  if (motoreadA == motoreadB) {
+    motor_count ++;
+  } else {
+    motor_count --;
   }
 }
