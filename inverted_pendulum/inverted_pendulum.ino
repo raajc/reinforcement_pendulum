f1a2843e89fcc206cbd23a83de06ba5e2c304fff
Raaj Chatterjee <raajc@sfu.ca>
2018-12-06 15:30:38 -0800

    Last code using interrupts for angle sensing

--- inverted_pendulum/inverted_pendulum.ino
+++ inverted_pendulum/inverted_pendulum.ino
@@ -1,23 +1,23 @@
 // Encoder pins
 const byte encoderPinA = 2;//outputA digital pin2
-const byte encoderPinB = 3;//outoutB digital pin3
+const byte encoderPinB = 13;//outoutB digital pin3
 const byte motorcoderPinA = 4;//outputA digital pin4
 const byte motorcoderPinB = 5;//outoutB digital pin5
 
 //Encoder counts
-volatile int count = 0;
+volatile int16_t count = 0;
 volatile int motor_count = 0;
 int protectedCount = 0;
 int previousCount = 0;
 
-double kp = 15;
+double kp = 10;
 double kd = 1;
 int offset = 0; //out of 255
 int diff = 0;
 int count_prev = 0;
 //Encoder Interrupts
 #define readA digitalRead(2)
-#define readB digitalRead(3)
+#define readB digitalRead(13)
 #define motoreadA digitalRead(4)
 #define motoreadB digitalRead(5)
 
@@ -28,11 +28,12 @@ int pwm_pinR = 7;
 // Variables
 int duty = 0;
 double output = 0;
-int control_loop_period = 2;
-int readout_loop_period = 50;
+int control_loop_period = 5;
+int readout_loop_period = 200;
 int control_loop_timer = 0;
 int readout_loop_timer = 0;
 int enter_flag = 0;
+char inByte = 0;
 
 void setup() {
 // PWM setup
@@ -42,10 +43,10 @@ pinMode(pwm_pinR, OUTPUT);
 //Encoder setup
 Serial.begin (115200);
 
-pinMode(encoderPinA, INPUT_PULLUP);
-pinMode(encoderPinB, INPUT_PULLUP);
-pinMode(motorcoderPinA, INPUT_PULLUP);
-pinMode(motorcoderPinB, INPUT_PULLUP);
+pinMode(encoderPinA, INPUT);
+pinMode(encoderPinB, INPUT);
+pinMode(motorcoderPinA, INPUT);
+pinMode(motorcoderPinB, INPUT);
 
 attachInterrupt(digitalPinToInterrupt(encoderPinA), isrA, CHANGE);
 attachInterrupt(digitalPinToInterrupt(encoderPinB), isrB, CHANGE);
@@ -69,34 +70,46 @@ if(millis()- control_loop_timer > control_loop_period) {
   diff = count - count_prev;
   if (count > 0) {
     output = kp*(count)+offset+kd*diff;
-    motorL(pwm_pinL, pwm_pinR, min(output,255));
+    motorL(pwm_pinL, pwm_pinR, output);
   } else {
     output = -kp*(count)+offset+kd*diff;
-    motorR(pwm_pinL, pwm_pinR, min(output, 255));
+    motorR(pwm_pinL, pwm_pinR, output);
   }
   count_prev = count;
 }
 
-if(millis() - readout_loop_timer> readout_loop_period) { 
+if(millis() - readout_loop_timer> readout_loop_period) {
+  while(Serial.available() > 0) {
+    char inByte = Serial.read();
+    if(inByte == 'R') {
+      count = -600;
+    }
+  }
   readout_loop_timer = millis();
   Serial.print("Encoder: ");
   Serial.print(count);
+  Serial.print(" Motor: ");
+  Serial.print(motor_count);
   Serial.print(" Output: ");
-  Serial.println(min(output,255));
+  Serial.print(min(output,255));
+  Serial.print(" Deriv: ");
+  Serial.print(diff*kd);
+  Serial.print(" Prop: ");
+  Serial.println(output-diff*kd);
 }
 
 }
 
 // Move cart left
 void motorL(int pwm_pinL, int pwm_pinR, int duty) {
-    analogWrite(pwm_pinL, duty);
+    analogWrite(pwm_pinL, min(output,255));
     analogWrite(pwm_pinR, 0); 
 }
 
 // Move cart right
 void motorR(int pwm_pinL, int pwm_pinR, int duty) {
     analogWrite(pwm_pinL, 0);
-    analogWrite(pwm_pinR, duty); 
+    analogWrite(pwm_pinR, min(output,255)); 
 }
 
 // Encoder ISRs
