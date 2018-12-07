cc0ff02f9ed3d106b9f2b526c23ffc06429c7dfd
Raaj Chatterjee <raajc@sfu.ca>
2018-12-06 16:03:28 -0800

    pong

--- inverted_pendulum/inverted_pendulum.ino
+++ inverted_pendulum/inverted_pendulum.ino
@@ -1,23 +1,17 @@
 // Encoder pins
-const byte encoderPinA = 2;//outputA digital pin2
-const byte encoderPinB = 13;//outoutB digital pin3
 const byte motorcoderPinA = 4;//outputA digital pin4
 const byte motorcoderPinB = 5;//outoutB digital pin5
 
 //Encoder counts
-volatile int16_t count = 0;
+int16_t count = 0;
 volatile int motor_count = 0;
-int protectedCount = 0;
-int previousCount = 0;
 
 double kp = 10;
 double kd = 1;
-int offset = 0; //out of 255
+int offset = 130; //out of 255
 int diff = 0;
 int count_prev = 0;
 //Encoder Interrupts
-#define readA digitalRead(2)
-#define readB digitalRead(13)
 #define motoreadA digitalRead(4)
 #define motoreadB digitalRead(5)
 
@@ -40,16 +34,26 @@ void setup() {
 pinMode(pwm_pinL, OUTPUT);
 pinMode(pwm_pinR, OUTPUT);
 
-//Encoder setup
 Serial.begin (115200);
 
-pinMode(encoderPinA, INPUT);
-pinMode(encoderPinB, INPUT);
+//Motor Encoder setup
 pinMode(motorcoderPinA, INPUT);
 pinMode(motorcoderPinB, INPUT);
 
-attachInterrupt(digitalPinToInterrupt(encoderPinA), isrA, CHANGE);
-attachInterrupt(digitalPinToInterrupt(encoderPinB), isrB, CHANGE);
+//Encoder Setup
+pmc_enable_periph_clk(ID_TC0);
+TC_Configure(TC0,0, TC_CMR_TCCLKS_XC0); 
+
+TC0->TC_BMR = TC_BMR_QDEN          // Enable QDEC (filter, edge detection and quadrature decoding)
+              | TC_BMR_POSEN       // Enable the position measure on channel 0
+              //| TC_BMR_EDGPHA    // Edges are detected on PHA only
+              //| TC_BMR_SWAP      // Swap PHA and PHB if necessary (reverse acting)
+              | TC_BMR_FILTER      // Enable filter
+              | TC_BMR_MAXFILT(63) // Set MAXFILT value
+             ;                       
+
+// Enable and trigger reset
+TC0->TC_CHANNEL[0].TC_CCR = TC_CCR_SWTRG | TC_CCR_CLKEN;
 
 attachInterrupt(digitalPinToInterrupt(motorcoderPinA), m_isrA, CHANGE);
 attachInterrupt(digitalPinToInterrupt(motorcoderPinB), m_isrB, CHANGE);
@@ -59,32 +63,32 @@ attachInterrupt(digitalPinToInterrupt(motorcoderPinB), m_isrB, CHANGE);
 void loop() {
   // put your main code here, to run repeatedly:
 
-  // Read Encoder
-  //noInterrupts();
-  //protectedCount = count;
-  //interrupts();
-
 if(millis()- control_loop_timer > control_loop_period) { 
+  count = REG_TC0_CV0-600;
   control_loop_timer = millis();
   // Print encoder if different from previous value
   diff = count - count_prev;
-  if (count > 0) {
+  if (count > 0 && count < 150 && abs(motor_count) < 80000) {
     output = kp*(count)+offset+kd*diff;
     motorL(pwm_pinL, pwm_pinR, output);
-  } else {
+  } else if (count <=0 && count > -150 && abs(motor_count) < 80000) {
     output = -kp*(count)+offset+kd*diff;
+    motorR(pwm_pinL, pwm_pinR, output);
+  } else {
+    output=0;
     motorR(pwm_pinL, pwm_pinR, output);
   }
   count_prev = count;
 }
 
 if(millis() - readout_loop_timer> readout_loop_period) {
-  while(Serial.available() > 0) {
-    char inByte = Serial.read();
-    if(inByte == 'R') {
-      count = -600;
-    }
-  }
+  count = REG_TC0_CV0-600;
+//  while(Serial.available() > 0) {
+//    char inByte = Serial.read();
+//    if(inByte == 'R') {
+//      REG_TC0_CV0 = -600;
+//    }
+//  }
   readout_loop_timer = millis();
   Serial.print("Encoder: ");
   Serial.print(count);
@@ -110,22 +114,6 @@ void motorL(int pwm_pinL, int pwm_pinR, int duty) {
 void motorR(int pwm_pinL, int pwm_pinR, int duty) {
     analogWrite(pwm_pinL, 0);
     analogWrite(pwm_pinR, min(output,255)); 
-}
-
-// Encoder ISRs
-void isrA() {
-  if(readB != readA) {
-    count ++;
-  } else {
-    count --;
-  }
-}
-void isrB() {
-  if (readA == readB) {
-    count ++;
-  } else {
-    count --;
-  }
 }
 
 // Motor Encoder ISRs
